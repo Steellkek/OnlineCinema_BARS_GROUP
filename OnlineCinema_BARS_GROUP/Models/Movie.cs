11034cdf4f60fb1da4b8962db63085bc48be54f4
Steellkek <alya.min80@gmail.com>
2022-04-13 18:37:09 +0300

    Создал класс Movie

---
+++ OnlineCinema_BARS_GROUP/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineCinema_BARS_GROUP.Models
+{
+    public class Movie
+    {
+        //id фильма в БД
+        public int Id {get; set;}
+        //название фильма
+        public string Name { get; set; }
+        //описание на главной страницу
+        public string ShortDesc { get; set; }
+        //описание на странице фильма
+        public string LongDesc { get; set; }
+        //Средняя оценка фильма
+        public double FilmScore { get; set; }
+        //ссылка на картинку постера фильма
+        public string Img { get; set; }
+        //ссылка на видеофайл фильма
+        public string Film { get; set; }
+        
+    }
+}
