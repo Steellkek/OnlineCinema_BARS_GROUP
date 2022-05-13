0f4d89575ae1885f4e9c8f3ea4d17d70973bba9b
Steellkek <alya.min80@gmail.com>
2022-05-13 11:58:10 +0300

    Для живого поиска добавил выделение

--- OnlineCinema_BARS_GROUP/wwwroot/js/Search.js
+++ OnlineCinema_BARS_GROUP/wwwroot/js/Search.js
@@ -4,24 +4,30 @@ window.onload=() =>{
     input.oninput=function (){
         let value = this.value.trim();
         let list = document.querySelectorAll(".col-3")
-        //console.log(list)
-        //console.log(list[0].getElementsByTagName('h5')[0].innerText)
         if(value){
             list.forEach(elem=>{
                 if(elem.getElementsByTagName('h5')[0].innerText.toLowerCase().search(value) ===-1){
                     elem.classList.add('hide')
+                    elem.getElementsByTagName('h5')[0].innerHTML=elem.getElementsByTagName('h5')[0].innerText;
                 }
                 else{
                     elem.classList.remove('hide')
+                    let str = elem.getElementsByTagName('h5')[0].innerText;
+                    elem.getElementsByTagName('h5')[0].innerHTML=InsertMark(str, elem.innerText.search(value),value.length)
                 }
             });
         }else
         {
             list.forEach(elem=>{
                 elem.classList.remove('hide')
+                elem.getElementsByTagName('h5')[0].innerHTML=elem.getElementsByTagName('h5')[0].innerText;
             })
         }
 
     }
+}
+
+function InsertMark(string, pos, len){
+    return string.slice(0, pos) + `<mark>`+string.slice(pos,pos+len)+`</mark>`+string.slice(pos+len)
 }
 
